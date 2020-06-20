07f557c412753f6415f6a0b0f8854d80ba2127b8
Dip-Ghosh <dipnsucse@gmail.com>
2020-06-20 15:42:13 +0600

    string literal

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+ //concept of let and conts
  let divs = document.getElementsByTagName('div');
  console.log(divs);
 for(let i = 0; i < divs.length; i++)
@@ -7,3 +8,34 @@ for(let i = 0; i < divs.length; i++)
         
     });
 }
+
+//string literals
+  function retrunEmployee()
+  {
+      const name = "Dip";
+      const age =  26;
+      const position = "Associate software Engineer";
+      console.log(`Name is ${name} and age is ${age} and positio is ${position}`);
+  }
+  retrunEmployee();
+
+  //object literals
+  const users = {
+      name:'Dip',
+      age : '21'
+  }
+  console.log(`name is ${users.name} and age is ${users.age}`);
+
+//user list
+let list = document.querySelector('.user_list');
+ function addUsers(name,lastName)
+ {
+    let  template = String.raw `
+     <div class="user">
+        <div>Name  ${name}</div>
+        <div>LastName ${lastName}</div>
+    </div>
+     `;
+     list.insertAdjacentHTML('beforeEnd',template);
+ }
+ addUsers('Dip ','Ghosh');
